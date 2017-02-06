91b2987ae45be3ed8fb5af262d5abb500d757a7c
Peteous <erik.peterman@und.edu>
2017-02-05 22:42:41 -0600

    Try to re-import tweety, except error

--- Bot.py
+++ Bot.py
@@ -4,6 +4,10 @@ except ImportError:
 	import os
 	os.system("python get-pip.py --user")
 	os.system("pip install --ignore-installed tweepy --user")
+	try:
+		import tweepy
+	except ImportError:
+		print("An error occurred with importing Twitter\'s API")
 
 # The consumer keys can be found on your application's Details
 # page located at https://dev.twitter.com/apps (under "OAuth settings")
