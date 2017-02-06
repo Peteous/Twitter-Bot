861cad3612e17ebb423dd0e23257088f0b7ef300
Peteous <erik.peterman@und.edu>
2017-02-06 15:02:09 -0600

    Add retweet and follow methods

--- Bot.py
+++ Bot.py
@@ -28,6 +28,38 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+for tweet in tweepy.Cursor(api.search, q='python').items(20):
+    try:
+        print('\nTweet by: @' + tweet.user.screen_name)
+
+        tweet.retweet()
+        print('Retweeted the tweet')
+
+        # Favorite the tweet
+        tweet.favorite()
+        print('Favorited the tweet')
+
+        time.sleep(5)
+
+    except tweepy.TweepError as e:
+        print(e.reason)
+
+    except StopIteration:
+        break
+
+for tweet in tweepy.Cursor(api.user_timeline).items(50):
+	try:
+		print(tweet)
+		if not tweet.favorited:
+			api.create_favorite(tweet.id)
+		if not tweet.retweeted:
+			api.retweet(tweet.id)
+		time.sleep(5)
+	except Exception as e:
+		print(e.reason)
+	except StopIteration:
+		break
+
 __tweetList = ["What is a git and how do I eat one? I'm a python.","Can a mouse be parsed? I'm a python.","I don't string parse, I slither. I'm a python.","Just adaded a commit to a repo. I'm a python."]
 __oopsList = ["Oops!","Oops","Gee Wiz!","Darn."]
 __errorOccured = ["Was error found.","I rattled my rattler.","Sssssthtthssss","I forgot parseltongue."]
