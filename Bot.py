6e7fb10324cda72057d1b7f0953aab4196f61426
Peteous <erik.peterman@und.edu>
2017-02-05 22:15:57 -0600

    try and catch import of tweepy

--- Bot.py
+++ Bot.py
@@ -1,1 +1,7 @@
-from tweepy import *
+try:
+	from tweepy import *
+except ImportError:
+	import os
+	os.system("python get-pip.py --user")
+	os.system("pip install --ignore-installed tweepy --user")
+
