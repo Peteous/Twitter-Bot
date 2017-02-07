5ebfb56a107d7459fc61af7c40b19baabf377347
Peteous <erik.peterman@und.edu>
2017-02-07 16:26:44 -0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -77,24 +77,24 @@ for tweet in tweepy.Cursor(api.search, q='python').items(50):
         break
 
 __tweetList = ["Snake, snake, Severus Snake. I'm a python.","What is a git and how do I eat one? I'm a python.","Can a mouse be parsed? I'm a python.","I don't string parse, I slither. I'm a python.","Just adaded a commit to a repo. I'm a python."]
-__oopsList = ["Oops!","Oops","Gee Wiz!","Darn."]
-__errorOccured = ["Was error found.","I rattled my rattler.","Sssssthtthssss","I forgot parseltongue."]
+__oopsList = ["Oops!","Oops","Gee Wiz!","Darn.","Oh deer."]
+__errorOccured = ["Was error found.","I rattled my rattler.","Sssssthtthssss","I forgot parseltongue.","I can't bot."]
 try:
-	for string in __tweetList:
-		api.update_status(string)
-		print(string)
-		__tweetList.remove(string)
-		__tweetList.append(string)
-		time.sleep(900)
+	api.update_status(string)
+	print(string)
+	__tweetList.remove(string)
+	__tweetList.append(string)
+	time.sleep(5)
 except tweepy.error.TweepError:
 	print('That tweet has already been tweeted')
 	try:
 		__status = __oopsList[randint(0,len(__errorOccured))] + " " + __errorOccured[randint(0,len(__errorOccured))] + " I'm a python."
 		api.update_status(__status)
-	except TweepError:
+	except tweepy.error.TweepError:
 		print('Something is boggled')
 		__status = ""
 		for index in range(0,randint(1,5)):
 			__status += "Bother "
 		__status += ". I'm a python."
 		api.update_status(__status)
+		print(__status)
