e98b18df69d3d1e0c713b09bf52f3347ba0b86b7
Peteous <erik.peterman@und.edu>
2017-02-08 14:15:10 -0600

    Comment code

--- Bot.py
+++ Bot.py
@@ -1,3 +1,4 @@
+# Try to import tweepy (twitter API wrapper), and pip install tweept if import fails
 try:
 	import tweepy
 except ImportError:
@@ -6,6 +7,7 @@ except ImportError:
 	os.system("pip install --ignore-installed tweepy --user")
 	try:
 		import tweepy
+	# If second import fails, write the failure to the console
 	except ImportError as e:
 		print("An error occurred with importing Twitter\'s API")
 		print(e.reason)
@@ -23,11 +25,12 @@ consumer_secret=""
 access_token=""
 access_token_secret=""
 
+# Set up tweepy object for user account
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
 
+# Find 50 most recent tweets from @UNDRAPTOR account and like & retweet them
 for tweet in tweepy.Cursor(api.user_timeline, screen_name='UNDRAPTOR').items(50):
 	try:
 		print('\nTweet direct from user: @' + tweet.user.screen_name)
@@ -47,6 +50,7 @@ for tweet in tweepy.Cursor(api.user_timeline, screen_name='UNDRAPTOR').items(50)
 	except StopIteration:
 		break
 
+# Find 50 most recent tweets in your main timeline and like & retweet them
 for tweet in tweepy.Cursor(api.home_timeline).items(50):
 	try:
 		print('\nTweet in timeline by: @' + tweet.user.screen_name)
@@ -66,6 +70,7 @@ for tweet in tweepy.Cursor(api.home_timeline).items(50):
 	except StopIteration:
 		break
 
+# Find 50 most recemt tweets containing "python" and like & retweet them
 for tweet in tweepy.Cursor(api.search, q='python').items(50):
     try:
         print('\nTweet by: @' + tweet.user.screen_name)
@@ -85,9 +90,14 @@ for tweet in tweepy.Cursor(api.search, q='python').items(50):
     except StopIteration:
         break
 
+# Set up list of original tweets
 __tweetList = ["Snake, snake, Severus Snake. I'm a python.","What is a git and how do I eat one? I'm a python.","Can a mouse be parsed? I'm a python.","I don't string parse, I slither. I'm a python.","Just adaded a commit to a repo. I'm a python."]
+
+# Set up list of exclamations and descriptors for random combination and tweeting
 __oopsList = ["Oops!","Oops","Gee Wiz!","Darn.","Oh deer."]
 __errorOccured = ["Was error found.","I rattled my rattler.","Sssssthtthssss","I forgot parseltongue.","I can't bot."]
+
+# try to tweet from __tweetList, of that fails, random combine words from __oopsList and __errorOccured list for tweeting
 try:
 	string = __tweetList[randint(0,len(__tweetList))]
 	api.update_status(string)
