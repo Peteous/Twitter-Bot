ed6849e3806d67ac8c6269e30701c72215ce3321
Peteous <erik.peterman@und.edu>
2017-02-16 14:01:28 -0600

    Updated AuthCodes class to parse and pass authorization codes

--- AuthCodes.py
+++ AuthCodes.py
@@ -2,29 +2,29 @@ class AuthCodes:
 	def __init__(self):
 		try:
 			with open('auth-codes.txt','r') as authfile:
-				consumer_key=authfile.readline()
-				consumer_secret=authfile.readline()
-				access_token=authfile.readline()
-				access_token_secret=authfile.readline()
+				self.consumer_key=authfile.readline()
+				self.consumer_secret=authfile.readline()
+				self.access_token=authfile.readline()
+				self.access_token_secret=authfile.readline()
 			authfile.close
 		except:
 			print('An error occured opening the required authorization file') 
 	def _stripID(self,text):
 		equals = 0
-		code = String()
-		for index in len(text):
-			if text[index] == '=' and not equals == 1:
+		code = ''
+		for index in range(len(text)):
+			if text[index] == '"' and not equals == 1:
 				equals = 1
-			if equals == 1:
+			elif equals == 1 and not text[index] == '"':
 				code += text[index]
-		return code
+		return code.rstrip()
 	def token_list(self):
 		return [self._stripID(consumer_key), self._stripID(consumer_secret), self._stripID(access_token), self._stripID(access_token_secret)]
 	def consumerkey(self):
-		return self._stripID(consumer_key)
+		return str(self._stripID(self.consumer_key))
 	def consumersecret(self):
-		return self._stripID(consumer_secret)
+		return str(self._stripID(self.consumer_secret))
 	def accesstoken(self):
-		return self._stripID(access_token)
+		return str(self._stripID(self.access_token))
 	def accesssecret(self):
-		return self._stripID(access_token_secret)
+		return str(self._stripID(self.access_token_secret))
